b0db73490b67c28051189567a46aed3b49539b4e
Edmar Zatarin <edzatarin@gmail.com>
2022-03-09 15:50:57 -0300

    Atualizado testes

--- run-test-coverage.cmd
+++ run-test-coverage.cmd
@@ -1,5 +1,5 @@
 dotnet test calculation.tests/calculation.tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 dotnet build-server shutdown
-dotnet sonarscanner begin /k:"calculation" /d:sonar.host.url=http://localhost:9000 /d:sonar.cs.opencover.reportsPaths="calculation.tests\coverage.opencover.xml" /d:sonar.coverage.exclusions="**Tests*.cs"
+dotnet sonarscanner begin /k:"calculation" /d:sonar.host.url=http://localhost:9000 /d:sonar.cs.opencover.reportsPaths="calculation.tests\coverage.opencover.xml" /d:sonar.login="b6c3e795a3fdfd96586ba3463307947da8fb4ca5" /d:sonar.coverage.exclusions="**Tests*.cs"/d:sonar.coverage.exclusions="**Tests*.cs" 
 dotnet build
-dotnet sonarscanner end
+dotnet sonarscanner /d:sonar.login="b6c3e795a3fdfd96586ba3463307947da8fb4ca5" end
